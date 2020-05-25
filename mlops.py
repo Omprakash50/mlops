c8f5e257bfaa91841fe16082c8386a3eb610ae42
Omprakash choudhari <56760981+Omprakash50@users.noreply.github.com>
2020-05-26 01:22:24 +0530

    Update mlops.py

--- mlops.py
+++ mlops.py
@@ -113,6 +113,6 @@ print(final_accuracy)
 
 import os
 if final_accuracy < 0.92:
-    os.system("curl --user '<jenkins username>:<jenkins_password>' http://192.168.99.102:8080/view/mlops/job/retrain/build?token=retrain")
+    os.system("curl --user 'admin:Omkar@jio.com1' http://192.168.43.60:8080/view/mlops/job/retrain/build?token=retrain")
 else:
     print("Your New accuracy=",final_accuracy)
