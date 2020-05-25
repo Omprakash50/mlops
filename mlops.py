3463c09ddbc06849a2377af8a52328efd6eac800
Omprakash choudhari <56760981+Omprakash50@users.noreply.github.com>
2020-05-26 00:55:10 +0530

    Update mlops.py

--- mlops.py
+++ mlops.py
@@ -111,8 +111,8 @@ print(final_accuracy)
 
 
 
-#import os
-#if final_accuracy < 0.92:
-#    os.system("curl --user '<jenkins username>:<jenkins_password>' http://192.168.99.102:8080/view/mlops/job/retrain/build?token=retrain")
-#else:
-#print("Your New accuracy=",final_accuracy)
+import os
+if final_accuracy < 0.92:
+    os.system("curl --user '<jenkins username>:<jenkins_password>' http://192.168.99.102:8080/view/mlops/job/retrain/build?token=retrain")
+else:
+print("Your New accuracy=",final_accuracy)
